6ab6bf6210cd9fd14cce506a296cf28bdab89d9e
HertBox <lavaltierra@gmail.com>
2016-04-05 18:55:57 -0500

    receve ping

--- ArduinoMAVLink/ArduinoMAVLink.ino
+++ ArduinoMAVLink/ArduinoMAVLink.ino
@@ -38,17 +38,19 @@ void loop() {
   Serial1.write(buf, len);
   /* The default UART header for your MCU */
   // Send the message (.write sends as bytes)
+  
   receve_msg();
 }
 
 void receve_msg()
-{ //receive data over serial
+{
+  //receive data over serial
+  
   while (Serial1.available() > 0)
   {
     mavlink_message_t msg;
     mavlink_status_t status;
     uint8_t rec =  Serial1.read();//Show bytes send from the pixhawk
-    //Serial.println(rec);
     //try to get a new message
     mavlink_parse_char(MAVLINK_COMM_0, rec, &msg, &status );
     
@@ -61,7 +63,15 @@ void receve_msg()
       Serial.println(msg.len);
       Serial.println("Msg Id: ");
       Serial.println(msg.msgid);
-    
+      Serial.println("Seq: ");
+      Serial.println(msg.seq);
+      Serial.println("payload: ");
+      Serial.println(msg.payload[MAVLINK_MAX_PAYLOAD_LEN]);
+      Serial.println("cka: ");
+      Serial.println(msg.ck_a);
+      Serial.println("ckb: ");
+      Serial.println(msg.ck_b);
+      
     // And get the next one
 
   }
