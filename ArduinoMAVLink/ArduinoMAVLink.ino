0c822e0abbb4c476dca1d8b08daccdadb89ab876
HertBox <lavaltierra@gmail.com>
2016-09-02 11:13:04 -0500

    Add files via upload
    this code almost works, I can read messages but cant select the one I want

--- ArduinoMAVLink/ArduinoMAVLink.ino
+++ ArduinoMAVLink/ArduinoMAVLink.ino
@@ -3,126 +3,125 @@
 #include <mavlink.h>
 #include <protocol.h>
 #include <math.h>
+//https://pixhawk.ethz.ch/mavlink/
 
-//#include "C:/Users/luis.Valtierra/Documents/ardupilot/libraries/mavlink/include/mavlink.h"         // Mavlink interface
-//#include "C:/Users/luis.Valtierra/Documents/ardupilot/libraries/mavlink/mavlink/v1.0/mavlink_types.h"
 
-uint8_t system_id = 1;
-uint8_t component_id = 1;
-uint8_t type = 6; //GCS
+uint8_t system_id = 100;
+uint8_t component_id = 50;
+uint8_t type = 0; //GCS
 uint8_t autopilot = 0; //generic
 
-
-
-
-uint8_t received_sysid = 1;
-uint8_t received_compid = 1;
+uint8_t received_sysid;//51 ;
+uint8_t received_compid;// = 68;
 uint8_t GCS_UNITS = 0;
 int a = 0;
+
 void setup() {
   Serial1.begin(57600); //RXTX from Pixhawk (Port 18,19 Arduino Mega)
   Serial.begin(57600); //Main serial port to read the port
 }
 
 void loop() {
-  // Define the system type (see mavlink_types.h for list of possible types)
-  receve_msg();
-  if (a == 0)
-  {
-    present_msg();
-    a = 1;
-  }
-  else {
-    a = 1;
-    start_feeds();
-  }
-}
+  // Define the system type (see mavlink_types.h for list of possible types) 
+    if (a==0) {
+     delay(2000);
+     present_msg();
+     delay(2000);
+     start_feeds();
+     a=1; 
+     }
+     delay(1000);
+     receve_msg();
+     }
+
+     
 void present_msg() {
   mavlink_message_t msg;
   mavlink_heartbeat_t heartbeat;
-  mavlink_msg_heartbeat_pack(system_id, component_id, &msg,  type, autopilot, 0, 0, 4);
-  send_message(&msg);
-  delay(10);
+  uint8_t buf[MAVLINK_MAX_PACKET_LEN];
+  mavlink_msg_heartbeat_pack(system_id, component_id, &msg, type, autopilot, 16, 0, 3);
+  //send_message(&msg);
+  uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
+  delay(1000);
+  Serial1.write(buf,len);
+ receve_msg();
 }
 
 
 void start_feeds() //request data
 {
   mavlink_message_t msg;
-  mavlink_msg_request_data_stream_pack(system_id, component_id, &msg,
-                                       received_sysid, received_compid, 30, 20, 1);
-  send_message(&msg);
-  delay(10);
-
-}
-
-void send_message(mavlink_message_t* msg) //send data bit by bit
-{
   uint8_t buf[MAVLINK_MAX_PACKET_LEN];
-  uint16_t len = mavlink_msg_to_send_buffer(buf, msg);
-  delay(1000);
-  Serial1.write(buf, len);// Send the message (.write sends as bytes)
+  mavlink_msg_request_data_stream_pack(system_id, component_id, &msg, received_sysid, received_compid, 30 ,1000, 1); //here its my problem the last digit
+//  send_message(&msg);
+  uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
+ delay(1000);
+  Serial1.write(buf,len);
+  
 }
 
-
-//void stop_feeds() //stop request
-//{
-//  mavlink_message_t msg1;
-//
-//  send_message(&msg1);
-//  delay(500);
-//}
-
+////////////////////////////////////////////////////////////////////////////////////////
 void receve_msg()
 { //receive data over serial
-  while (Serial1.available() > 0)
-  {
+ // Serial.println(Serial1.available());
+ //  Serial.println(" ");
+  while (Serial1.available() > 0){   
     mavlink_message_t msg;
     mavlink_status_t status;
     uint8_t rec =  Serial1.read();//Show bytes send from the pixhawk
-    Serial.println(rec);
-    mavlink_parse_char(Serial1, rec, &msg, &status);
-
-    gcs_handleMessage(&msg);
-
-
+   // Serial.print("len = ");
+ //   Serial.print(" Data = ");
+ //   Serial.println(rec);
+    if(mavlink_parse_char(MAVLINK_COMM_0, rec, &msg, &status)){ 
+      
+      Serial.println(" ");
+      Serial.print("Mensaje: ");
+      Serial.println(msg.msgid);
+      gcs_handleMessage(&msg);
+    }
   }
-  Serial.println("sali");
-
 }
 
 void gcs_handleMessage(mavlink_message_t* msg) //read varaible
 {
   //  Serial.println();
-  //  Serial.print("Message ID: ");
-  //  Serial.println(msg->msgid);
+  // Serial.print("Message ID: ");
+  // Serial.println(msg->msgid);
   switch (msg->msgid) {
     case MAVLINK_MSG_ID_HEARTBEAT:
       {
         mavlink_heartbeat_t packet;
         mavlink_msg_heartbeat_decode(msg, &packet);
-        uint8_t beat = 1;
         if ((*msg).sysid != 0xff) { // do not process mission planner heartbeats if we have two receiver xbees
-          uint8_t received_sysid = (*msg).sysid; // save the sysid and compid of the received heartbeat for use in sending new messages
-          uint8_t received_compid = (*msg).compid;
-          uint8_t bmode = packet.base_mode;
-          uint8_t cmode = packet.custom_mode;
-          Serial.println("Im in heartbeat");
-
+          received_sysid = (*msg).sysid; // save the sysid and compid of the received heartbeat for use in sending new messages
+          received_compid = (*msg).compid;
+          Serial.println("heartbeat");
         }
-        break;
+        break; 
       }
     case MAVLINK_MSG_ID_ATTITUDE:
       {
         // decode
         mavlink_attitude_t packet;
         mavlink_msg_attitude_decode(msg, &packet);
-        uint8_t pitch = packet.pitch;
-        uint8_t yaw = packet.yaw;
-        uint8_t roll = packet.roll;
-        Serial.println("Im im  Attitude");
+        float pitch = packet.pitch;
+        float yaw = packet.yaw;
+        float roll = packet.roll;
+        Serial.println("Im in  Attitude");
+        Serial.print("pitch: ");
+        Serial.println(pitch);
+        Serial.print("yaw: ");
+        Serial.println(yaw);
+        Serial.print("roll: ");      
+        Serial.println(roll);
         break;
       }
+      case MAVLINK_MSG_ID_PING:
+      {
+        Serial.println("System_time");
+        break;
+        }
+      
     case MAVLINK_MSG_ID_GPS_RAW_INT:
       {
         // decode
@@ -132,6 +131,7 @@ void gcs_handleMessage(mavlink_message_t* msg) //read varaible
         uint8_t  mav_utime = packet.time_usec;
         uint8_t  numSats = packet.satellites_visible;
         uint8_t  cog = packet.cog;
+        Serial.println("GPS");
         break;
       }
     case MAVLINK_MSG_ID_GLOBAL_POSITION_INT:
@@ -143,12 +143,14 @@ void gcs_handleMessage(mavlink_message_t* msg) //read varaible
         uint8_t  longitude = packet.lon;
         if (GCS_UNITS == 0)uint8_t altitude = packet.alt / 1000;
         else if ( (GCS_UNITS == 1) || (GCS_UNITS == 2) ) uint8_t altitude = (packet.alt / 1000) * 3.28084;
+        Serial.println("msg");
         break;
       }
     case MAVLINK_MSG_ID_GPS_STATUS:
       {
         mavlink_gps_status_t packet;
         mavlink_msg_gps_status_decode(msg, &packet);
+        Serial.println("GPS STATUS");
         break;
       }
     case MAVLINK_MSG_ID_VFR_HUD:
@@ -164,6 +166,7 @@ void gcs_handleMessage(mavlink_message_t* msg) //read varaible
         else if (GCS_UNITS == 2)uint8_t grs = packet.groundspeed * 1.94;
         if (GCS_UNITS == 0)uint8_t vsi = packet.climb;
         else if ( (GCS_UNITS == 1) || (GCS_UNITS == 2) )uint8_t vsi = packet.climb * 3.28084;
+        Serial.println("id vfr");
         break;
       }
     case MAVLINK_MSG_ID_RAW_PRESSURE:
@@ -171,6 +174,7 @@ void gcs_handleMessage(mavlink_message_t* msg) //read varaible
         // decode
         mavlink_raw_pressure_t packet;
         mavlink_msg_raw_pressure_decode(msg, &packet);
+        Serial.println("raw preasure");
         break;
       }
     case MAVLINK_MSG_ID_SYS_STATUS:
@@ -179,6 +183,7 @@ void gcs_handleMessage(mavlink_message_t* msg) //read varaible
         mavlink_sys_status_t packet;
         mavlink_msg_sys_status_decode(msg, &packet);
         uint8_t  vbat = packet.voltage_battery;
+        Serial.print("Status");
         break;
       }
   }
